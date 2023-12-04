b3a5433842ebe70a5a6e048f5d72537c128e1c93
Farid Ghorbani <127934668+Faridghr@users.noreply.github.com>
2023-12-04 00:50:23 -0800

    Implement util package - DatabaseConnector - addUserOwnedBook,getAllBook,getUserOwnedBook

--- src/util/DatabaseConnector.java
+++ src/util/DatabaseConnector.java
@@ -4,7 +4,13 @@
  */
 package util;
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import model.User;
 import model.Book;
 import model.LoanRequest;
@@ -22,13 +28,77 @@ public class DatabaseConnector {
 
     private DatabaseConnector() {
     }
-  
     
-    //addUserOwnedBook(user, book)
+        //addUserOwnedBook(user, book)
+    public static void addUserOwnedBook(User user, Book book) {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            String sql = "INSERT INTO UserBooks (user_id, book_id) VALUES (?, ?)";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, user.getUserId());
+                preparedStatement.setInt(2, book.getBookId());
+                preparedStatement.executeUpdate();
+                System.out.println("Book added to user's owned books successfully!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     //getUserOwnedBook(user)
+    public static List<Book> getUserOwnedBooks(User user) {
+        List<Book> ownedBooks = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            String sql = "SELECT Books.* FROM Books JOIN UserBooks ON Books.book_id = UserBooks.book_id WHERE UserBooks.user_id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, user.getUserId());
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        int bookId = resultSet.getInt("book_id");
+                        String title = resultSet.getString("title");
+                        String author = resultSet.getString("author");
+                        String description = resultSet.getString("description");
+                        boolean availability = resultSet.getBoolean("availability");
+
+                        Book book = new Book(bookId, title, author, description, availability);
+                        ownedBooks.add(book);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ownedBooks;
+    }
+  
     //getAllBook()
-    //searchByTitle(title)
-    //searchByAuthor(author)
+    public static List<Book> getAllBooks() {
+        List<Book> allBooks = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            String sql = "SELECT * FROM Books";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        int bookId = resultSet.getInt("book_id");
+                        String title = resultSet.getString("title");
+                        String author = resultSet.getString("author");
+                        String description = resultSet.getString("description");
+                        boolean availability = resultSet.getBoolean("availability");
+
+                        Book book = new Book(bookId, title, author, description, availability);
+                        allBooks.add(book);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allBooks;
+    }
+    
+    //browsBookByTitle(title)
+    
+    
+    
+    //browsBookByAuthor(author)
     //getUserBorrowedBook(user)
     //addUserBorrowedBook(user, book)
     //removeUserBorrowedBook(user, bookID)
